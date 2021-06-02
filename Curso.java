63b493ed7ad5dc76d1a9db66b0e853531bec91e7
JMxnuell <Jsuarez1804@alumno.ipn.mx>
2021-06-02 06:55:17 -0500

    clases terminadas

--- Curso.java
+++ Curso.java
@@ -1,7 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Curso {
     
     private Materia materia;
     private Profesor profesor;
+    private int cupo;
+    private ArrayList<Inscripcion> alumnosInscritos;
     
+    
+    public Curso(Materia materia, Profesor profesor, int cupo){
+        this.materia = materia;
+        this.profesor = profesor;
+        this.cupo = cupo;
+        alumnosInscritos = new ArrayList<>();
+    }
+
+    public List<Inscripcion> getAlumnosInscritos() {
+        return alumnosInscritos;
+    }
+    public void setAlumnosInscritos(ArrayList<Inscripcion> alumnosInscritos) {
+        this.alumnosInscritos = alumnosInscritos;
+    }
+    public int getCupo() {
+        return cupo;
+    }
+    public void setCupo(int cupo) {
+        this.cupo = cupo;
+    }
+    public Materia getMateria() {
+        return materia;
+    }
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }public Profesor getProfesor() {
+        return profesor;
+    }
+    public void setProfesor(Profesor profesor) {
+        this.profesor = profesor;
+    }
 
 }
